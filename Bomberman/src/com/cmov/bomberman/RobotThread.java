880c2eb70a9925112506a02d15ad51da190a4a05
Gureya Daharewa Davis <gureya.daharewa@gmail.com>
2014-04-17 02:38:16 +0100

    Robotmovements modified

--- Bomberman/src/com/cmov/bomberman/RobotThread.java
+++ Bomberman/src/com/cmov/bomberman/RobotThread.java
@@ -1,11 +1,21 @@
 package com.cmov.bomberman;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Random;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 
 public class RobotThread extends Thread {
+
+	List<Integer> moveablepos;
+	private Random randomGenerator;
 
 	private IMoveableRobot robotActiviy;
 	public Activity activity;
@@ -13,8 +23,6 @@ public class RobotThread extends Thread {
 	private int col = 0;
 	private Byte[][] GridLayout = null;
 	private LogicalWorld logicalworld;
-	private boolean movingRight;
-	private boolean movingUp;
 	private boolean running = false;
 	@SuppressLint("UseSparseArrays")
 	private Map<Integer, Integer> updatedRobotPos = new HashMap<Integer, Integer>();
@@ -34,100 +42,68 @@ public class RobotThread extends Thread {
 	}
 
 	private void move(int x, int y) {
-		
-		int nyr = y+1;
-		int nyl = y-1;
-		
-		int nxu = x-1;
-		int nxd = x+1;
-		
-		Cell cellr = logicalworld.getElement(x, nyr)[0];
-		Cell celll = logicalworld.getElement(x, nyl)[0];
-		
-		Cell cellu = logicalworld.getElement(nxu, y)[0];
-		Cell celld = logicalworld.getElement(nxd, y)[0];
-		
-		if (nyl > 0 || nyr < this.col || nxu > 0 || nxd < this.row ) {
-			
-			if (cellr == null) {
-				movingRight = true;
-			} else if(celll == null) {
-				movingRight = false;
-			}
-			else if(cellu == null)
-				movingUp = true;
-			else if(celld == null)
-				movingUp = false;
-		}
-		
-		if (movingRight) {
-			System.out
-					.println("cell is empty lets move the robot,, remove this sysout..");
-			ConfigReader.gridlayout[x][y] = '-';
-			this.logicalworld.setElement(x, y, 0, null);
-			updatedRobotPos.put(x, nyr);
-			movingRight = false;
-		} else if (movingUp) {
-			System.out
-					.println("cell is empty lets move the robot,, remove this sysout..");
-			ConfigReader.gridlayout[x][y] = '-';
-			this.logicalworld.setElement(x, y, 0, null);
-			updatedRobotPos.put(nxu, y);
-			movingUp = false;
-		} else if (!movingRight) {
-			System.out
-					.println("cell is empty lets move the robot,, remove this sysout..");
-			ConfigReader.gridlayout[x][y] = '-';
-			this.logicalworld.setElement(x, y, 0, null);
-			updatedRobotPos.put(x, nyl);
-			movingRight = false;
-		}
-		else if (!movingUp) {
-			System.out
-					.println("cell is empty lets move the robot,, remove this sysout..");
-			ConfigReader.gridlayout[x][y] = '-';
-			this.logicalworld.setElement(x, y, 0, null);
-			updatedRobotPos.put(nxd, y);
-			movingUp = false;
-		}
-		
-		/* else if (++ny < this.col) {
-			Cell cell = logicalworld.getElement(x, ny)[0];
-			if (cell == null) {
+
+		moveablepos = new ArrayList<Integer>();
+		randomGenerator = new Random();
+
+		int nyr = y + 1;
+		int nyl = y - 1;
+
+		int nxu = x - 1;
+		int nxd = x + 1;
+
+		if (nyl > 0 || nyr < this.col || nxu > 0 || nxd < this.row) {
+
+			Cell cellr = logicalworld.getElement(x, nyr)[0]; // Move right = 1
+			Cell celll = logicalworld.getElement(x, nyl)[0]; // Move left = 2
+
+			Cell cellu = logicalworld.getElement(nxu, y)[0]; // Move up = 3
+			Cell celld = logicalworld.getElement(nxd, y)[0]; // Move down = 4
+
+			if (cellr == null)
+				moveablepos.add(1);
+			if (celll == null)
+				moveablepos.add(2);
+			if (cellu == null)
+				moveablepos.add(3);
+			if (celld == null)
+				moveablepos.add(4);
+
+			int index = randomGenerator.nextInt(moveablepos.size());
+			int pos = moveablepos.get(index);
+            
+			if (pos == 1) {
 				System.out
 						.println("cell is empty lets move the robot,, remove this sysout..");
 				ConfigReader.gridlayout[x][y] = '-';
 				this.logicalworld.setElement(x, y, 0, null);
-				updatedRobotPos.put(x, ny);
+				updatedRobotPos.put(x, nyr);
+			} else if (pos == 2) {
+				System.out
+						.println("cell is empty lets move the robot,, remove this sysout..");
+				ConfigReader.gridlayout[x][y] = '-';
+				this.logicalworld.setElement(x, y, 0, null);
+				updatedRobotPos.put(x, nyl);
+			} else if (pos == 3) {
+				System.out
+						.println("cell is empty lets move the robot,, remove this sysout..");
+				ConfigReader.gridlayout[x][y] = '-';
+				this.logicalworld.setElement(x, y, 0, null);
+				updatedRobotPos.put(nxu, y);
+			} else if (pos == 4) {
+				System.out
+						.println("cell is empty lets move the robot,, remove this sysout..");
+				ConfigReader.gridlayout[x][y] = '-';
+				this.logicalworld.setElement(x, y, 0, null);
+				updatedRobotPos.put(nxd, y);
 			}
-		}*/
-		 
-	}
+			else
+			{
+				return;
+			}
+			moveablepos.clear();
+		}
 
-	public void moveCircle() {
-
-		/*
-		 * for (int x = 0; x < this.row; x++) { for (int y = 0; y < this.col;
-		 * y++) {
-		 * 
-		 * switch (GridLayout[x][y]) { case 'r': { move(x,y); } break; } } }
-		 * this.x++; if ( < 0 || ny < 0 || this.logicalWorld.getWidth() <= ny ||
-		 * this.logicalWorld.getHeight() <= nx) return false; // player can not
-		 * move if there is wall or obstacle , have to add here. Cell el =
-		 * this.logicalWorld.getElement(nx, ny)[0]; if (el == null) // oder
-		 * Extra {
-		 * 
-		 * } if (this.movingRight) { this.x++; ConfigReader.gridlayout[x][y] =
-		 * 'r'; robotActiviy.RobotMovedAtLogicalLayer(x, y); } else { this.x--;
-		 * ConfigReader.gridlayout[x][y] = 'r';
-		 * robotActiviy.RobotMovedAtLogicalLayer(x, y); }
-		 * 
-		 * if (this.x == width) { this.movingRight = false;
-		 * 
-		 * } else if (this.x == 0) { this.movingRight = true;
-		 * 
-		 * }
-		 */
 	}
 
 	public void setRunning(boolean running) {
